084e9960bfece2adae6cee61d5a46c3089085ce8
Chad Granum <exodist7@gmail.com>
2016-12-20 18:13:52 -0800

    Automated Version Bump

--- lib/Term/Table.pm
+++ lib/Term/Table.pm
@@ -2,7 +2,7 @@ package Term::Table;
 use strict;
 use warnings;
 
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 
 use Term::Table::Cell();
 
--- lib/Term/Table/CellStack.pm
+++ lib/Term/Table/CellStack.pm
@@ -2,7 +2,7 @@ package Term::Table::CellStack;
 use strict;
 use warnings;
 
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 
 use Term::Table::HashBase qw/-cells -idx/;
 
